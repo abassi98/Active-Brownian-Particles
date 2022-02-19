8923934c3bf9e60c446701976795db7b0d62e876
lupoalberto98 <alberto.bassi.3@studenti.unipd.it>
2022-02-19 14:34:44 +0100

    Add keywords arguments to pybind.cpp

--- src/pybind.cpp
+++ src/pybind.cpp
@@ -14,15 +14,15 @@ PYBIND11_MODULE(abp, m) {
     py::class_<point>(m, "point")
         .def_readwrite("x", &point::x)
         .def_readwrite("y", &point::y)
-        .def(py::init<double, double>())
-        .def("distance_to_point", &point::distance_to_point);
+        .def(py::init<double, double>(), py::arg("x"), py::arg("y"))
+        .def("distance_to_point", &point::distance_to_point,  py::arg("position"));
         
     // Derived class region
     py::class_<region, point>(m, "region")
-        .def(py::init<double, double, double>())
+        .def(py::init<double, double, double>(), py::arg("x"), py::arg("y"),  py::arg("radius"))
         .def_readwrite("radius", &region::radius);
 
-    m.def("getVector", &getVector, "Compute the vector(point) connecting two points");
+    m.def("getVector", &getVector, "Compute the vector(point) connecting two points",  py::arg("end"),  py::arg("start"));
 
     // Class active brownian particle
     py::class_<ABP_2d>(m, "ABP_2d")
@@ -44,16 +44,18 @@ PYBIND11_MODULE(abp, m) {
         .def_readwrite("w", &ABP_2d::w)
         .def_readwrite("reactant", &ABP_2d::reactant)
         // Functions
-        .def(py::init<const region&,const region&, unsigned&, double&, double&, double&, double&, double&, double&, double&, double&>())
-        .def("apply_pbc", &ABP_2d::apply_pbc)
-        .def("pbc_distance", &ABP_2d::pbc_distance)
-        .def("potential", &ABP_2d::potential)
-        .def("compute_force", &ABP_2d::compute_force)
-        .def("position_step", &ABP_2d::position_step)
-        .def("theta_step", &ABP_2d::theta_step)
-        .def("is_near_minimum", &ABP_2d::is_near_minimum)
-        .def("is_inside_region", &ABP_2d::is_inside_region)
+        .def(py::init<const region&,const region&, unsigned&, double&, double&, double&, double&, double&, double&, double&, double&>(),
+        py::arg("reactant"), py::arg("target"), py::arg("num_steps"), py::arg("dt"), py::arg("v"), py::arg("D_r"), py::arg("D_theta"),
+        py::arg("k"), py::arg("L"), py::arg("mu"), py::arg("w"))
+        .def("apply_pbc", &ABP_2d::apply_pbc, py::arg("position"))
+        .def("pbc_distance", &ABP_2d::pbc_distance, py::arg("A"), py::arg("B"))
+        .def("potential", &ABP_2d::potential, py::arg("position"))
+        .def("compute_force", &ABP_2d::compute_force, py::arg("position"))
+        .def("position_step", &ABP_2d::position_step, py::arg("position"), py::arg("theta"), py::arg("noise_x"), py::arg("noise_y"))
+        .def("theta_step", &ABP_2d::theta_step, py::arg("theta"), py::arg("noise_theta"))
+        .def("is_near_minimum", &ABP_2d::is_near_minimum, py::arg("position"))
+        .def("is_inside_region", &ABP_2d::is_inside_region, py::arg("position"), py::arg("target"))
         .def("dynamics", &ABP_2d::dynamics)
-        .def("print_dynamics", &ABP_2d::print_dynamics)
-        .def("print_bool_dynamics", &ABP_2d::print_bool_dynamics);
+        .def("print_dynamics", &ABP_2d::print_dynamics, py::arg("filename"))
+        .def("print_bool_dynamics", &ABP_2d::print_bool_dynamics, py::arg("filename"));
 }
